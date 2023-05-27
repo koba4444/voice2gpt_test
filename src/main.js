65698e67cfbe94a70a67fba3fa671324572b64e9
kok4444 <koba4444@gmail.com>
2023-05-27 23:02:14 +0300

    short version working

--- src/main.js
+++ src/main.js
@@ -7,9 +7,6 @@ import { TELEGRAM_TOKEN, OPENAI_API_KEY, TEST_ENV} from "../constants/index.js";
 import {ogg} from "./ogg.js"
 import { openai } from "./openai.js";
 
-
-
-
 console.log("HIHIHIHIIHIHIHIHIH")
 const INITIAL_SESSION = {
     messages: [],
@@ -30,41 +27,22 @@ bot.command("start", async(ctx) => {
     ctx.session = INITIAL_SESSION;
     await ctx.reply("Жду вашего голосового или текстового сообщения");
 });
-bot.on(message("voice"), async(ctx) => {
+
+bot.on(message("text"), async(ctx) => {
     console.log(ctx.session);
     ctx.session ??= INITIAL_SESSION;
-    try{
-        //await ctx.reply("Я вас услышал и жду ответа от OpenAI")
-        await ctx.reply(code("Я вас услышал и жду ответа от OpenAI"));
-        //await ctx.reply(JSON.stringify(ctx.message.voice, null, 2));
-        const link = await ctx.telegram.getFileLink(ctx.message.voice.file_id);
-        const userId = String(ctx.message.from.id);
+    await ctx.reply(code("Я тут"));
+});
 
-        console.log(link.href);
-        const oggPath = await ogg.create(link.href, userId);
-        const mp3Path = await ogg.toMp3(oggPath, userId);
-        const text = await openai.transcription(mp3Path);
-        await ctx.reply(code(`Ваш вопрос: ${text}`));
-        ctx.session.messages.push({role: openai.roles.USER, content: text});
-        //const messages = [{role: openai.roles.USER, content: text}]
-
-
-
-        const response = await openai.chat(ctx.session.messages);
-
-        ctx.session.messages.push({role: openai.roles.ASSISTANT, content: response.data.choices[0].message.content});
-        console.log({role: openai.roles.ASSISTANT, content: response.data.choices[0].message.content});
-        console.log(ctx.session.messages);
-
-        await ctx.reply(response.data.choices[0].message.content);
-
-    } catch{
-        console.log("error while  receiving voice message", e.message);
-    }
-    await ctx.reply("Чем я могу помочь?");
-    //await ctx.reply("You said something", { reply_to_message_id: ctx.message.message_id });
-})
-
+async (req, res) => {
+  try {
+    await bot.handleUpdate(req.body, res);
+    await ctx.reply(code(`Ваш вопрос: ${ctx.message.text}`));
+  } catch (err) {
+    console.error(err);
+    res.status(500).end();
+  }
+};
 bot.launch();
 process.once("SIGINT", () => bot.stop("SIGINT"));
 process.once("SIGTERM", () => bot.stop("SIGTERM"));
